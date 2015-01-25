2af658f2e5a5b14aade92908451b081cba99ec84
elenadmitrieva <elena.an.dmitrieva@gmail.com>
2015-01-25 15:25:44 +0200

    Implementation  of sum of the squares of the two largest numbers. Version 2.

--- sum_2_max_square.rb
+++ sum_2_max_square.rb
@@ -10,3 +10,23 @@ def sum_2_max_square_v1(a, b, c)
   end
   a**2 + b**2
 end
+
+def sum_2_max_square_v2(a, b, c)
+  array = Array.new
+  array << a << b << c
+  i = 0
+  l = array.length
+  while i < l
+    j = i + 1
+    while j < l
+      if array[i] < array[j]
+        variable = array[i]
+        array[i] = array[j]
+        array[j] = variable
+      end
+      j += 1
+    end
+    i += 1
+  end
+  array[0].to_i**2 + array[1].to_i**2
+end
